47274e188240a0a430f2935e17ed288fb152009d
unknown <mohit.p@admybrand.com>
2023-04-04 16:10:56 +0530

    home page added

--- src/components/organisms/footer.tsx
+++ src/components/organisms/footer.tsx
@@ -4,11 +4,11 @@ import Image from "next/image";
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-50 text-black">
+    <footer className="w-[100vw] mt-[100vh] bg-gray-50 text-black border-t border-gray-800">
       <div className="container mx-auto py-6">
         <div className="lg:flex">
           <div className="lg:w-1/4">
-            <h2 className="text-lg font-medium mb-4">
+            <h2 className="text-lg font-medium mb-4 ml-20 items-center">
               <Image
                 alt="logo"
                 src={"/footerlogo.svg"}
@@ -16,129 +16,120 @@ const Footer = () => {
                 height={100}
               />
             </h2>
-            <div className="flex items-center mb-4">
-              <a href="#" className="text-gray-100 hover:text-white mr-4">
-                <Image alt="logo" src={"/discord.svg"} width={50} height={50} />
+            <div className="flex items-center align-center text-center mb-3">
+              <a href="#" className="text-gray-100 hover:text-white mr-8 ml-8 items-center">
+                <Image alt="logo" src={"/discord.svg"} width={25} height={25} />
               </a>
-              <a href="#" className="text-gray-100 hover:text-white mr-4">
-                {/* <FaTwitter size={24} /> */}
+              <a href="#" className="text-gray-100 hover:text-white mr-8">
+              <Image alt="logo" src={"/instagram.svg"} width={25} height={25} />
+              </a>
+              <a href="#" className="text-gray-100 hover:text-white mr-8">
+              <Image alt="logo" src={"/twitter.svg"} width={25} height={25} />
               </a>
               <a href="#" className="text-gray-100 hover:text-white">
-                {/* <FaInstagram size={24} /> */}
+              <Image alt="logo" src={"/youtube.svg"} width={25} height={25} />
               </a>
             </div>
           </div>
-          <div className="lg:w-1/4">
-            <h2 className="text-lg font-medium mb-4">Heading 1</h2>
-            <ul className="list-reset mb-4">
-              <li className="mt-2">
-                <Link href="/about">
-                  <li>About Us</li>
-                </Link>
-              </li>
-              <li className="mt-2">
-                <Link href="/services">
-                  <li>Our Services</li>
-                </Link>
-              </li>
-              <li className="mt-2">
-                <Link href="/team">
-                  <li>Our Team</li>
-                </Link>
-              </li>
-              <li className="mt-2">
-                <Link href="/career">
-                  <li>Careers</li>
-                </Link>
-              </li>
-              <li className="mt-2">
-                <Link href="/contact">
-                  <li>Contact Us</li>
-                </Link>
-              </li>
-            </ul>
-          </div>
-          <div className="lg:w-1/4">
-            <h2 className="text-lg font-medium mb-4">Heading 2</h2>
-            <ul className="list-reset mb-4">
-              <li className="mt-2">
+          <div className="lg:w-1/4 border-l border-gray-800 ">
+            <h2 className="text-lg font-medium ml-4 mb-4">HOW TO JOIN</h2>
+            <ul className="list-reset mb-4 ml-4">
+            <li className="mt-2">
                 <Link href="/blog">
-                  <li>Blog</li>
+                  <li>Mission</li>
                 </Link>
               </li>
               <li className="mt-2">
                 <Link href="/faq">
-                  <li>FAQ</li>
+                  <li>Adguin Bases</li>
                 </Link>
               </li>
               <li className="mt-2">
                 <Link href="/privacy">
-                  <li>Privacy Policy</li>
+                  <li>History</li>
                 </Link>
               </li>
               <li className="mt-2">
                 <Link href="/terms">
-                  <li>Terms of Use</li>
+                  <li>FAQs</li>
                 </Link>
               </li>
               <li className="mt-2">
                 <Link href="/sitemap">
-                  <li>Sitemap</li>
+                  <li>News & Events</li>
+                </Link>
+              </li>
+              
+            </ul>
+          </div>
+          <div className="lg:w-1/4 border-l border-gray-800 ">
+            <h2 className="text-lg font-medium ml-4 mb-4">HOW TO JOIN</h2>
+            <ul className="list-reset mb-4 ml-4">
+            <li className="mt-2">
+                <Link href="/about">
+                  <li>Career Finder</li>
+                </Link>
+              </li>
+              <li className="mt-2">
+                <Link href="/services">
+                  <li>Education</li>
+                </Link>
+              </li>
+              <li className="mt-2">
+                <Link href="/team">
+                  <li>Benefits</li>
+                </Link>
+              </li>
+              <li className="mt-2">
+                <Link href="/career">
+                  <li>Training</li>
+                </Link>
+              </li>
+              <li className="mt-2">
+                <Link href="/contact">
+                  <li>More Info</li>
                 </Link>
               </li>
             </ul>
           </div>
-          <div className="lg:w-1/4">
-            <h2 className="text-lg font-medium mb-4">Heading 3</h2>
-            <ul className="list-reset mb-4">
+          <div className="lg:w-1/4 border-l border-gray-800 ">
+            <h2 className="text-lg font-medium ml-4 mb-4">HOW TO JOIN</h2>
+            <ul className="list-reset mb-4 ml-4">
               <li className="mt-2">
                 <Link href="/support">
-                  <li>Support</li>
+                  <li>How to join</li>
                 </Link>
               </li>
               <li className="mt-2">
                 <Link href="/feedback">
-                  <li>Feedback</li>
+                  <li>Apply Now</li>
                 </Link>
               </li>
               <li className="mt-2">
                 <Link href="/newsletter">
-                  <li>Newsletter</li>
+                  <li>Application</li>
                 </Link>
               </li>
               <li className="mt-2">
                 <Link href="/siteupdates">
-                  <li>Site Updates</li>
+                  <li>Future Joining</li>
                 </Link>
               </li>
               <li className="mt-2">
                 <Link href="/press">
-                  <li>Press</li>
+                  <li>More Info</li>
                 </Link>
               </li>
             </ul>
           </div>
         </div>
-        <div className="border-t border-gray-800 pt-4 mt-4 flex flex-col items-center">
-          <p className="text-sm text-gray-100 mb-2">
-            © 2023 My Awesome Company. All rights reserved.
-          </p>
-          <p className="text-sm text-gray-100">
-            Built with{" "}
-            <a
-              href="https://nextjs.org/"
-              className="text-gray-100 hover:text-white"
-            >
-              Next.js
-            </a>{" "}
-            and{" "}
-            <a
-              href="https://tailwindcss.com/"
-              className="text-gray-100 hover:text-white"
-            >
-              Tailwind CSS
-            </a>
-            .
+        <div className="border-t bg-blue border-gray-800 pt-4 flex flex-col items-center">
+          <p className="text-sm text-black mb-2 bg-blue">
+            Copyright © 2023 Adguin Ltd. All rights reserved.
+          
+            <div className="inline-block text-lg text-black pl-40">Privacy & Policy</div>
+            <div className="inline-block text-lg text-black pl-40">News & Events</div>
+            <div className="inline-block text-lg text-black pl-40">Cookie Settings</div>
           </p>
         </div>
       </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,7 +14,72 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}></main>
+      <main className="w-[100vw] h-[50vh] absolute bg-none z-10">
+        <div className="bg-white"></div>
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <div className=" max-w-6xl w-full px-4">
+        {/* First Row */}
+        <div className="flex flex-col items-center justify-center mt-20 mb-16">
+          <h1 className="text-8xl font-bold text-gray-800 text-center mb-4 font-abandoned">
+            THE BEST PRIVACY ONLINE
+          </h1>
+          <h2 className="text-4xl font-light text-gray-800 text-center font-miriam">
+          Browse privately. Search privately. And ditch Big Tech.
+          </h2>
+        </div>
+
+        {/* Second Row */}
+        <div className="flex items-center justify-center mb-16">
+          {/* <button className="py-4 px-8 rounded-lg bg-blue-600 text-white font-semibold shadow-md hover:bg-blue-700"> */}
+          <button className="bg-violet-700 text-2xl font-miriam w-[250px] hover:bg-violet-500 text-white font-light py-2 px-4 rounded">
+            Continue As Guest
+          </button>
+        </div>
+
+        {/* Third Row */}
+        <div className="bg-gray-700 bg-blur bg-opacity-25 rounded-3xl py-8 px-4 lg:px-16">
+          <div className="flex flex-col lg:flex-row lg:justify-between">
+            {/* First Column */}
+            <div className="flex flex-col items-center lg:w-1/3 lg:pr-4">
+              <h3 className="text-lg font-semibold text-gray-800 font-miriam mb-4">
+                USERS EVERY MONTH
+              </h3>
+              <p className="text-5xl font-abandoned text-white">
+                25K+
+              </p>
+            </div>
+
+            {/* Second Column */}
+            <div className="border-l border-gray-800 flex flex-col items-center lg:w-1/3 lg:px-4 mt-8 lg:mt-0">
+              <h3 className="text-lg font-semibold text-gray-800 font-miriam mb-4">
+                TRACKERS BLOCKED
+              </h3>
+              <p className="text-5xl font-abandoned text-white">
+                1,35L+
+              </p>
+            </div>
+
+            {/* Third Column */}
+            <div className="border-l border-gray-800 flex flex-col items-center lg:w-1/3 lg:pl-4 mt-8 lg:mt-0">
+              <h3 className="text-lg font-semibold text-gray-800 font-miriam mb-4">
+                COMPANIES JOINED
+              </h3>
+              <p className="text-5xl font-abandoned text-white">
+                8K+
+              </p>
+            </div>
+          </div>
+          <div className="border-gray-800 mt-8 border-t items-center">
+          <button className="bg-black text-2xl font-miriam w-[250px] items-center hover:bg-gray-800 pt-2 pb-2 ml-[19vw] mt-8 text-white font-light rounded">
+            Get Started
+          </button>
+
+          </div>
+        </div>
+      </div>
+    </div>
+    {/* <Image src={'/bg.png'} alt="bg-image" className="relative w-[100vw] z-0 top-0 h-[120vh] bg-white" width="1000" height="1000" /> */}
+      </main>
     </>
   );
 }
